91bf9501ab08e5baabd2eb05526bb1136300d640
henuzxy <30334058+zhao5502169@users.noreply.github.com>
2019-08-19 19:58:37 +0800

    SplayTreeSet and MySTL

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # MySTL
 Self-implemented Standard Template Library
+
+自己实现的一些STL库的一些东西，最近整理一下，并且扩展了一些东西，不过也有功能没有实现。
+
+### SplayTreeSet
+
+对应STL库的set,利用Splay来实现。插入，删除均摊时间复杂度是O(longN)。对于大量重复的数据，速度会更快，因为频率最高的value在Splay是出现在根节点周围的。这里的Splay是一个去重的容器。
+
+这里是一些共有方法，使用户可以调用。
+
+| 成员方法      | 作用                    |
+| ------------- | ----------------------- |
+| clear()       | 清空所有数据            |
+| insert(value) | 插入数据key             |
+| erase(value)  | 删除数据                |
+| size()        | 返回容器数据个数        |
+| count(value)  | 返回value在容器中的个数 |
+
---
+++ SplayTreeSet/SplayTreeSet.h
@@ -0,0 +1,141 @@
+#pragma once
+#include<vector>
+#include<memory>
+using namespace std;
+#define lson (0)
+#define rson (1)
+template<typename T>
+class TreeNode{
+public:
+    typedef int size_type;
+    template<typename T1>
+    friend class SplayTreeSet;
+    TreeNode(T _value,shared_ptr<TreeNode<T>> _father = nullptr){
+        value = _value;
+        father = _father;
+        son[0] = son[1] = nullptr;
+    }
+private:
+    size_type cnt;
+    T value;
+    shared_ptr<TreeNode<T>> son[2];
+    weak_ptr<TreeNode<T>> father;
+};
+template<typename T1>
+class SplayTreeSet{
+public:
+    typedef int size_type;
+    typedef T1 value_type;
+    typedef TreeNode<T1> Node;
+    SplayTreeSet(){
+        Size = 0;
+        root = nullptr;
+    }
+    void clear(){
+        Size = 0;
+        root = nullptr;
+    }
+    void insert(value_type val){
+        if(root == nullptr){
+            Size++;
+            root = make_shared<Node>(Node(val));
+        }
+        shared_ptr<Node> t = root;
+        while(t){
+            if(t->value == val){
+                splay(t,nullptr);
+                return;
+            }
+            bool which = (val > t->value);
+            if(t->son[which] == nullptr){
+                t->son[which] = make_shared<Node>(val,t);
+                Size++;
+            }
+            t = t->son[which];
+        }
+    }
+    size_type size(){
+        return Size;
+    }
+    size_type count(value_type val){
+        shared_ptr<Node> t = root;
+        while(t){
+            if(t->value == val){
+                splay(t,nullptr);
+                return t->cnt;
+            }
+            t = t->son[val > t->value];
+        }
+        return 0;
+    }
+    bool erase(value_type val){
+        shared_ptr<Node> t = where_value(val);
+        if(t == nullptr)
+            return false;
+        size--;
+        splay(t,nullptr);
+        if(t->son[lson] == nullptr){
+            root = t->son[rson];
+            if(root)
+                root->father = nullptr;
+        }
+        else{
+            shared_ptr<Node> p = t->son[lson];
+            while(p->son[rson] != nullptr)
+                p = p->son[rson];
+            splay(p,t);
+            root = p;
+            root->father = nullptr;
+            p->son[rson] = t->son[rson];
+            if(p->son[rson])
+                p->son[rson]->father = p;
+        }
+        return true;
+    }
+private:
+    shared_ptr<Node> root;
+    size_type Size;
+    bool which_son(shared_ptr<Node> f,shared_ptr<Node> s){
+        return f->son[rson] == s;
+    }
+    void rotate(shared_ptr<Node> t){
+        shared_ptr<Node> f = t->father.lock();
+        shared_ptr<Node> g = f->father.lock();
+        bool which = which_son(f,t);
+        f->son[which] = t->son[!which];
+        t->son[!which] = f;
+        f->father = t;
+        t->father = g;
+        if(f->son[which])
+            f->son[which]->father = f;
+        if(g)
+            g->son[which_son(g,f)] = t;
+        else
+            root = t;
+    }
+    void splay(shared_ptr<Node> t,shared_ptr<Node> p){
+        while(t->father.lock() != p){
+            shared_ptr<Node> f = t->father.lock();
+            shared_ptr<Node> g = f->father.lock();
+            if(g == p)
+                rotate(t);
+            else{
+                if(which_son(g,f) == which_son(f,t)){
+                    rotate(f);rotate(t);
+                }
+                else{
+                    rotate(t);rotate(t);
+                }
+            }
+        }
+    }
+    shared_ptr<Node> where_value(value_type _value){
+        shared_ptr<Node> t = root;
+        while(t){
+            if(t->value == _value)
+                return t;
+            t = t->son[_value > t->value];
+        }
+        return nullptr;
+    }
+};
