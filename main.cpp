6c1454f4b95432a50dacde2855860f3a2ae79e3a
zhaoxiaoyu <zhaoxiaoyu.henuzxy@bytedance.com>
2020-03-10 22:04:00 +0800

    feat: 支持CMAKE

--- main.cpp
+++ main.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
-#include <pthread.h>
-#include <stdlib.h>
-#include <string>
-#include <iostream>
-#include "SmartPointer/SmartPointer.h"
-int main(void){
-    SmartPointer<int> p1;
-    SmartPointer<int> p2(new int(2));
-    p1 = p2;
+#include "Base/defer.h"
 
-   // shared_ptr<int> ptr2 = ptr1;
- //   std::cout << ptr1.use_count() << std::endl;
+using namespace std;
+
+int main(void){
+    int x[2] = {1,2};
+    auto [a,b] = x;
+    cout << a << " " << b << endl;
     return 0;
 }
